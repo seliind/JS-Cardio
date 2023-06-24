e400bb53e4a3b3ac518d4b971be29a654434bba6
selin <selinshere@gmail.com>
2023-06-24 15:41:54 +0300

    double-click-heart-created

---
+++ 10_Double_Click_Heart_Animation/script.js
@@ -0,0 +1,16 @@
+const image = document.querySelector(".container");
+const heartIcon = document.querySelector(".heart");
+
+image.addEventListener("dblclick", (e) => {
+
+    let xValue = e.clientX - e.target.offsetLeft;
+    let yValue = e.clientY - e.target.offsetTop;
+    heartIcon.style.left= `${xValue}px`
+    heartIcon.style.top= `${yValue}px`
+
+  heartIcon.classList.add("active");
+
+  setTimeout(() => {
+    heartIcon.classList.remove("active");
+  }, 3000);
+});
